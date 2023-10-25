4d80ff832031399c196d706d6e314dce92bb7f57
Avinash Chaurasiya <chaurasiyaavinash97@gmail.com>
2023-10-25 18:36:51 +0530

    update variable name

--- application/views/product/order/view.php
+++ application/views/product/order/view.php
@@ -31,71 +31,71 @@
                     'autocomplete' => false, 'id' => 'edit_product', 'method' => 'post'
                 ], [
                     'type' => $this->url_encrypt->encode('tbl_product'),
-                    'id' => $Product->id
+                    'id' => $product->id
                 ])
                 ?>
                 <div class="form-group row">
                     <div class="col-md-3">
                         <label for="name">User Name</label>
-                        <input class="form-control" type="text" name="first_name" value="<?= $Product->first_name . ' '. $Product->last_name ?>" id="name" readonly>
+                        <input class="form-control" type="text" name="first_name" value="<?= $product->first_name . ' '. $product->last_name ?>" id="name" readonly>
                     </div>
                     <div class="col-md-3">
                         <label for="email">Email</label>
-                        <input class="form-control" type="text" name="email" value="<?= $Product->email ?>" id="email" readonly>
+                        <input class="form-control" type="text" name="email" value="<?= $product->email ?>" id="email" readonly>
                     </div>
                     <div class="col-md-3">
                         <label for="phone">Phone</label>
-                        <input class="form-control" type="text" name="phone" value="<?= $Product->phone ?>" id="phone" readonly>
+                        <input class="form-control" type="text" name="phone" value="<?= $product->phone ?>" id="phone" readonly>
                     </div>
                     <div class="col-md-3">
                         <label for="name">Country</label>
-                        <input class="form-control" type="text" name="country" value="<?= $Product->country ?>" id="country" readonly>
+                        <input class="form-control" type="text" name="country" value="<?= $product->country ?>" id="country" readonly>
                     </div>
                 </div>
                 <div class="form-group row">
                     <div class="col-md-3">
                         <label for="address">Address</label>
-                        <input class="form-control" type="text" name="address" value="<?= $Product->address ?>" id="name" readonly>
+                        <input class="form-control" type="text" name="address" value="<?= $product->address ?>" id="name" readonly>
                     </div>
                     <div class="col-md-3">
                         <label for="city">City</label>
-                        <input class="form-control" type="text" name="city" value="<?= $Product->city ?>" id="city" readonly>
+                        <input class="form-control" type="text" name="city" value="<?= $product->city ?>" id="city" readonly>
                     </div>
                     <div class="col-md-3">
                         <label for="postal_code">Postal Code</label>
-                        <input class="form-control" type="text" name="postal_code" value="<?= $Product->postal_code ?>" id="postal_code" readonly>
+                        <input class="form-control" type="text" name="postal_code" value="<?= $product->postal_code ?>" id="postal_code" readonly>
                     </div>
                     <div class="col-md-3">
                         <label for="state">State</label>
-                        <input class="form-control" type="text" name="state" value="<?= $Product->state ?>" id="state" readonly>
+                        <input class="form-control" type="text" name="state" value="<?= $product->state ?>" id="state" readonly>
                     </div>
                 </div>
                 <div class="form-group row">
                     <div class="col-md-3">
                         <label for="name">Product Name</label>
-                        <input class="form-control" type="text" name="name" value="<?= $Product->product_name ?>" id="name" readonly>
+                        <input class="form-control" type="text" name="name" value="<?= $product->product_name ?>" id="name" readonly>
                     </div>
                     <div class="col-md-3">
                         <label for="qty">Qty.</label>
-                        <input class="form-control" type="number" Placeholder="Price" value="<?= $Product->quantity ?>" name="qty" id="prcie" readonly>
+                        <input class="form-control" type="number" Placeholder="Price" value="<?= $product->quantity ?>" name="qty" id="prcie" readonly>
                     </div>
                     <div class="col-md-3">
                         <label for="qty">Cost.</label>
-                        <input class="form-control" type="text" Placeholder="Cost" value="<?= $Product->cost ?>" name="cost" id="cost" readonly>
+                        <input class="form-control" type="text" Placeholder="Cost" value="<?= $product->cost ?>" name="cost" id="cost" readonly>
                     </div>
                     <div class="col-md-3">
                         <label for="payment_mode">Paymet Mode.</label>
-                        <input class="form-control" type="text" Placeholder="Payment Mode" value="<?= $Product->payment_mode ?>" name="payment_mode" id="payment_mode" readonly>
+                        <input class="form-control" type="text" Placeholder="Payment Mode" value="<?= $product->payment_mode ?>" name="payment_mode" id="payment_mode" readonly>
                     </div>
                 </div>
                 <div class="form-group row">
                     <div class="col-md-3">
                         <label for="name">Color</label>
-                        <input class="form-control" type="text" name="color" value="<?= getColorNameByNumber($Product->color) ?>" id="color" readonly>
+                        <input class="form-control" type="text" name="color" value="<?= getColorNameByNumber($product->color) ?>" id="color" readonly>
                     </div>
                     <div class="col-md-3">
                         <label for="qty">Size.</label>
-                        <input class="form-control" type="text" Placeholder="Size" value="<?= getSizeNameByNumber($Product->size) ?>" name="size" id="size" readonly>
+                        <input class="form-control" type="text" Placeholder="Size" value="<?= getSizeNameByNumber($product->size) ?>" name="size" id="size" readonly>
                     </div>
                 </div>
 
@@ -123,8 +123,8 @@
     <script>
         $(document).ready(() => {
             $('.select2').select2();
-            <?php if ($Product->image) { ?>
-                $("#imgPreview").attr("src", '<?= base_url('uploads/images/') . $Product->image ?>');
+            <?php if ($product->image) { ?>
+                $("#imgPreview").attr("src", '<?= base_url('uploads/images/') . $product->image ?>');
                 $("#imgPreview").show()
             <?php } ?>
         });
